4994d0caceee85ab9fb4ffaf1fbf2b63816f25c6
Pygone <pygonebe@outlook.com>
2023-12-11 18:45:31 +0800

    lab2-3 finish

--- kernel/strap.c
+++ kernel/strap.c
@@ -65,9 +65,21 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+	{
+		// Allocate a new physical page
+		struct Page *new_page = alloc_page();
+		if (!new_page) {
+			panic("Page allocation failed.\n");
+		}
 
-      break;
+		// Map the new page to the virtual address that caused the page fault
+		if (map_pages(current->pagetable, ROUNDDOWN(stval,PGSIZE), PGSIZE, (uint64)new_page, prot_to_type(
+				PROT_WRITE | PROT_READ, 1)) != 0) {
+			panic("Page mapping failed.\n");
+		}
+
+		break;
+	}
     default:
       sprint("unknown page fault.\n");
       break;
