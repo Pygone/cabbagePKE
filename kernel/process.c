8b4fcdc9f2d6b8062f81ae353e0b08126aa2b1e3
pygone <pygonebe@outlook.com>
2024-03-04 14:53:27 +0800

    to pass

--- kernel/process.c
+++ kernel/process.c
@@ -234,7 +234,7 @@ int do_fork( process* parent)
 			  parent->mapped_info[i].npages * PGSIZE,
 			  lookup_pa(parent->pagetable, parent->mapped_info[i].va),
 			  prot_to_type(PROT_READ | PROT_EXEC, 1));
-
+        sprint("do_fork map code segment at pa:%lx of parent to child at va:%lx.\n", lookup_pa(parent->pagetable, parent->mapped_info[i].va), parent->mapped_info[i].va);
         // after mapping, register the vm region (do not delete codes below!)
         child->mapped_info[child->total_mapped_region].va = parent->mapped_info[i].va;
         child->mapped_info[child->total_mapped_region].npages =
